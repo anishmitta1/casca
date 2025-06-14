748407e2649457608c3299fedc215fa210989841
Anish Mitta <anishmitta@Anishs-MacBook-Pro.local>
2025-06-14 14:02:54 -0700

    wip

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -2,12 +2,12 @@ import { redirect } from "next/navigation";
 
 import { createClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
-import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
 
   const { data, error } = await supabase.auth.getUser();
+
   if (error || !data?.user) {
     redirect("/auth/login");
   }
@@ -26,10 +26,6 @@ export default async function ProtectedPage() {
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
           {JSON.stringify(data.user, null, 2)}
         </pre>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { DeployButton } from "@/components/deploy-button";
 import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import Link from "next/link";
 
 export default function Home() {
@@ -21,11 +19,7 @@ export default function Home() {
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            <SignUpUserSteps />
-          </main>
+          hi hello
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
